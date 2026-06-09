c46632966dd11a87c04dace8f8dbca4a39e94853
ElianZ407 <aburtoelian3@gmail.com>
2026-06-08 23:39:43 -0600

    feat(US-E3): implement queryTransactions logic with type filtering, sorting and pagination

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TokenService } from './token.service';
 
 @Controller('tokens')
@@ -8,14 +8,12 @@ export class TokenController {
   @Get(':mint')
   async getTokenInfo(@Param('mint') mint: string) {
     const data = await this.tokenService.getTokenInfo(mint);
-    if (!data) throw new NotFoundException('Token mint not found');
     return { success: true, data };
   }
 
   @Get(':mint/holders')
   async getTokenHolders(@Param('mint') mint: string) {
     const data = await this.tokenService.getTokenHolders(mint);
-    if (!data) throw new NotFoundException('Token mint not found');
     return { success: true, data };
   }
 }
